a4ae50e6c35d09462f50bd22d417e1443e8c1da2
François de taxis du Poët <francois.de-taxis-du-poet@epitech.eu>
2020-10-21 14:24:36 +0200

    navbar stub user component stub

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <!--    <img alt="Vue logo" src="./assets/logo.png">-->
+        <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+        <Navbar/>
+        <User msg="CECI EST UN TEST DU COMPONENT USER" currentUser="CURRENT USER TEST"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    // import HelloWorld from './components/HelloWorld.vue'
+    import User from './components/User.vue'
+    import Navbar from './components/Navbar.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            // HelloWorld,
+            User,
+            Navbar
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
